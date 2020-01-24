da8b3edd4b1307089f8cb720596f5e6d55c3ed5c
APGLn <584647996@qq.com>
2020-01-24 10:25:45 +0800

    初步的qq登陆

--- meiduo_mall/meiduo_mall/static/js/register.js
+++ meiduo_mall/meiduo_mall/static/js/register.js
@@ -52,7 +52,7 @@ var vm = new Vue({
             // 生成一个编号 : 严格一点的使用uuid保证编号唯一， 不是很严谨的情况下，也可以使用时间戳
             this.image_code_id = this.generateUUID();
             // 设置页面中图片验证码img标签的src属性
-            this.image_code_url = 'http://127.0.0.1:8000' + "/image_codes/" + this.image_code_id + "/";
+            this.image_code_url = this.host + "/image_codes/" + this.image_code_id + "/";
             console.log(this.image_code_url);
         },
         // 检查用户名
